e98098aab723083c129a5719e62cbdfb1b142a6c
Jeff Ross <jross323@gmail.com>
2018-08-25 11:25:11 -0700

    implement auto-play

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Tic-Tac-Toe
 
-This is an implementation of Tic-Tac-Toe that runs in the browser.
+#### Overview
+
+This is an implementation of Tic-Tac-Toe that runs in the browser. Implemented using vanilla HTML5/CSS3/ES6
 
 See and play the game at: https://jeffro94.github.io/tictactoe-web/
+
+#### Rules for AI
+
+Level 1
+- Randomly select an available space
+
+Level 2
+- Try the center space
+- Try the corner spaces (in random order)
+- Try the side spaces (in random order)
+
+Level 3
+- Not implemented
--- t3.js
+++ t3.js
@@ -18,42 +18,105 @@ document.querySelectorAll(".game-cell").forEach(function(cell) {
 document.getElementById("btnPlayAgain").addEventListener("click", playAgain);
 
 // this function handles a cell-click event. if the game is not over
-// and the cell is currently empty, update the cell contents and check 
-// if the game is over
+// and the cell is currently empty, make the move
 //
 function cellClicked(e) {
-    if (!(gameOver) && (!(e.target.innerText))) {
-        // update the cell with the appropriate move
-        e.target.innerText = playerChars[playerTurn];
+    let autoPlayOn = document.getElementById("autoPlay").checked;
 
+    if (!(gameOver) && (!(e.target.innerText)) && (!(autoPlayOn && (playerTurn == 2)))) {
         let movePosition = JSON.parse(e.target.dataset.position);
-        let winningSpaces = checkForWinner(playerTurn, movePosition[0], movePosition[1]);
 
-        if (winningSpaces) {
-            document.getElementById("info-message").innerText = `Player ${playerTurn} wins!`;
+        makeMove(playerTurn, movePosition[0], movePosition[1]);
+    }
+}
 
-            // disable the board and enable reset option
-            gameOver = true;
-            document.querySelector(".game").classList.add("win");
+function makeAIMove(difficulty) {
+    let moveRow;
+    let moveCol;
 
-            // highlight the winning spaces
-            winningSpaces.forEach(function(cell) {
-                document.querySelector(`.game-cell[data-position="[${cell[0]},${cell[1]}]`).classList.add("winning-space");
-            })
+    if (difficulty == 1) {
+        // Level 1: choose a random cell that is not currently occupied
+        do {
+            moveRow = Math.floor(Math.random() * 3);
+            moveCol = Math.floor(Math.random() * 3);
+        } while (document.querySelector(`.game-cell[data-position="[${moveRow},${moveCol}]`).innerText == "");
+    } 
+    else {
+        // Level 2: First, try the middle spot. if occupied then,
+        //  try the 4 corners in a random order, if occupied
+        //  try the four center-side spots in a random order
+        moveRow = moveCol = 1;
+        let validMove = false;
+        if (document.querySelector(`.game-cell[data-position="[${moveRow},${moveCol}]`).innerText == "") validMove = true;
+
+        if (!validMove) {
+            let corners = [[0,0], [0,2], [2,0], [2,2]];
+            shuffleArray(corners); // randomize the order;
+            for (let i = 0; i < 4; i++) {
+                if (document.querySelector(`.game-cell[data-position="[${corners[i][0]},${corners[i][1]}]`).innerText == "") {
+                    moveRow = corners[i][0];
+                    moveCol = corners[i][1];
+                    validMove = true;
+                    break;
+                }
+            }
         }
-        else if (checkForStalemate()) {
-            document.getElementById("info-message").innerText = `The game has ended in a stalemate.`;
 
-            // disable the board and enable reset option
-            gameOver = true;
-            document.querySelector(".game").classList.add("tie");
+        if (!validMove) {
+            let sides = [[0,1], [1,2], [2,1], [1,0]];
+            shuffleArray(sides); // randomize the order;
+            for (let i = 0; i < 4; i++) {
+                if (document.querySelector(`.game-cell[data-position="[${sides[i][0]},${sides[i][1]}]`).innerText == "") {
+                    moveRow = sides[i][0];
+                    moveCol = sides[i][1];
+                    validMove = true;
+                    break;
+                }
+            }
         }
-        else {
-            // set the next player as active
-            playerTurn = (playerTurn === 1) ? 2 : 1;
+    }
+    
+    makeMove(2,moveRow,moveCol);
+}
 
-            // update the status message
-            document.getElementById("info-message").innerText = `It's Player ${playerTurn}'s turn.`;
+// this function makes a move for the specified player and location
+// this includes checking for winner, stalement, and updating the player
+// next up, and triggering the AI-move for player 2 if it's enabled
+//
+function makeMove(player, moveRow, moveCol) {
+    // update the cell with the appopriate move
+    document.querySelector(`.game-cell[data-position="[${moveRow},${moveCol}]`).innerText = playerChars[player];
+
+    let winningSpaces = checkForWinner(player, moveRow, moveCol);
+
+    if (winningSpaces) {
+        document.getElementById("info-message").innerText = `Player ${player} wins!`;
+
+        // disable the board and enable reset option
+        gameOver = true;
+        document.querySelector(".game").classList.add("win");
+
+        // highlight the winning spaces
+        winningSpaces.forEach(function(cell) {
+            document.querySelector(`.game-cell[data-position="[${cell[0]},${cell[1]}]`).classList.add("winning-space");
+        })
+    }
+    else if (checkForStalemate()) {
+        document.getElementById("info-message").innerText = `The game has ended in a stalemate.`;
+
+        // disable the board and enable reset option
+        gameOver = true;
+        document.querySelector(".game").classList.add("tie");
+    }
+    else {
+        // set the next player as active
+        playerTurn = (player === 1) ? 2 : 1;
+
+        // update the status message
+        document.getElementById("info-message").innerText = `It's Player ${playerTurn}'s turn.`;
+
+        if ((playerTurn == 2) && document.getElementById("autoPlay").checked) {
+            window.setTimeout(makeAIMove, 500, document.getElementById("difficulty").value);
         }
     }
 }
@@ -79,7 +142,7 @@ function playAgain() {
  * @param {number} player - The player number of the current player to check
  * @param {number} moveRow - The row index of the last move
  * @param {number} moveCol - The column index of the last move
- * @returns {Object} array of winning space indexes, otherwise nothing
+ * @returns {Object} array of the winning spaces if yes, otherwise nothing
  */
 function checkForWinner(player, moveRow, moveCol) {
     var winningSpaces = []; // keep track of the potential winning spaces
@@ -132,4 +195,13 @@ function checkForStalemate() {
         if (cell.innerText == "") freeSpaces++;
     });
     return (freeSpaces == 0); // it's a stalemate if there are no remaining free spaces
+}
+
+// function to randomly sort an array. source: 
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
